35a8a9d98a6b05b08f2741741112191fd85e45c7
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-25 21:23:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicios-python-orientacao-objetos-2
+ Exercícios relacionados ao curso de Python orientado a objetos nível 2 da Alura.
---
+++ modelo.py
@@ -0,0 +1,50 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+vingadores = Filme('Vingadores - Ultimato', 2019, 180)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} min - Likes: {vingadores.likes}')
+
+atlanta = Serie('Atlanta', 2016, 3)
+print(f'Nome: {atlanta.nome} - Ano {atlanta.ano} - Temporadas {atlanta.temporadas} - Likes: {atlanta.likes}')
