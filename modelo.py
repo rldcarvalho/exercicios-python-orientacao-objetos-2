6db7c067952367e1ddfd1f06cd27dea86a200a4e
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-26 22:51:27 -0300

    Refatoramento de código
    Aplicação de polimorfismo;
    Criação da representação textual do objeto;
    Implementação de uma herança interável.

--- modelo.py
+++ modelo.py
@@ -19,20 +19,56 @@ class Programas:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-class Filme:
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} likes'
+
+class Filme(Programas):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
-class Serie:
+
+class Serie(Programas):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 
 vingadores = Filme('Vingadores - Ultimato', 2019, 180)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} min - Likes: {vingadores.likes}')
-
 atlanta = Serie('Atlanta', 2016, 3)
-print(f'Nome: {atlanta.nome} - Ano {atlanta.ano} - Temporadas {atlanta.temporadas} - Likes: {atlanta.likes}')
+tmep = Filme('Todo mundo em Pânico', 2000, 84)
+demolidor = Serie('Demolidor', 2015, 3)
+
+vingadores.dar_like()
+vingadores.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
